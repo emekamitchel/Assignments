c418658f4ce0db9b6df0362447be350cbe8e01bf
emekamitchel <48516991+emekamitchel@users.noreply.github.com>
2020-03-29 07:50:32 +0100

    Add files via upload

---
+++ JS_Task1.js
@@ -0,0 +1,17 @@
+var name = "Mitchy";
+console.log("My name is: " + name);
+
+var courses = ["Html", "Css", "Javascript", "Python"];
+
+const data = [];
+
+ for (let i=0; i<=100; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+}
+
+console.log(data);
